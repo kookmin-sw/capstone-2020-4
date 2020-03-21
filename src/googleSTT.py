bd2cbd0a0534a3608edfaca750a446e01cc6bc4c
leejuhyoung11 <srlee96@naver.com>
2020-03-21 14:12:13 +0900

    Create googleSTT.py

---
+++ src/googleSTT.py
@@ -0,0 +1,83 @@
+#-*-coding:utf-8-*-
+import io
+import os
+import json
+from makeData import writeCSV
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
+def transcribe_gcs(gcs_uri):
+    """Asynchronously transcribes the audio file specified by the gcs_uri."""
+
+    client = speech.SpeechClient()
+    audio = types.RecognitionAudio(uri=gcs_uri)
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=44100,
+        language_code='ko-KR',
+        audio_channel_count=2)
+    operation = client.long_running_recognize(config, audio)
+    print('Waiting for operation to complete...')
+    response = operation.result(timeout=90)
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        print(u'Transcript: {}'.format(result.alternatives[0].transcript))
+        #print('Confidence: {}'.format(result.alternatives[0].confidence))
+        #writeCSV(result.alternatives[0].transcript)  #save csv file
+
+def sample_recognize(local_file_path):
+    """
+    Transcribe a short audio file using synchronous speech recognition
+
+    Args:
+      local_file_path Path to local audio file, e.g. /path/audio.wav
+    """
+
+    client = speech.SpeechClient()
+
+    # local_file_path = 'resources/brooklyn_bridge.raw'
+
+    # The language of the supplied audio
+    language_code = "ko-KR"
+
+    # Sample rate in Hertz of the audio data sent
+    sample_rate_hertz = 44100
+
+    # Encoding of audio data sent. This sample sets this explicitly.
+    # This field is optional for FLAC and WAV audio formats.
+    encoding = enums.RecognitionConfig.AudioEncoding.LINEAR16
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=44100,
+        language_code='ko-KR',
+        audio_channel_count=2)
+
+    with io.open(local_file_path, "rb") as f:
+        content = f.read()
+    audio = {"content": content}
+
+    response = client.recognize(config, audio)
+    for result in response.results:
+        # First alternative is the most probable result
+        alternative = result.alternatives[0]
+        print(u"Transcript: {}".format(alternative.transcript))
+
+
+def implicit():
+    from google.cloud import storage
+
+    # If you don't specify credentials when constructing the client, the
+    # client library will look for credentials in the environment.
+    storage_client = storage.Client('sunlit-liberty-268205')
+
+    # Make an authenticated API request
+    buckets = list(storage_client.list_buckets())
+    print(buckets)
+
+
+#implicit()
+#transcribe_gcs('gs://youtubespeech/theaudio.wav')
+sample_recognize('C:\\Users\\01097\\PycharmProjects\\untitled\\test03.wav')
