6f051b730be06a912f35a9c4741f5008cc2b87df
leejuhyoung11 <srlee96@naver.com>
2020-03-30 16:59:24 +0900

    Update makeData.py

--- src/makeData.py
+++ src/makeData.py
@@ -3,8 +3,19 @@ import csv
 def writeCSV(sentence):
     f = open('C:\\Users\\01097\\PycharmProjects\\untitled\\csv\\write.csv', 'a', newline='')
     wr = csv.writer(f)
-    wr.writerow(["1", sentence])
+    wr.writerow([sentence])
 
 
     f.close()
 
+def printTime(t):
+    f = open('C:\\Users\\01097\\PycharmProjects\\untitled\\csv\\write.csv', 'a', newline='')
+    wr = csv.writer(f)
+    wr.writerow([str(t) + ":00 ~ " + str(t+5) + ":00"])
+
+
+    f.close()
+
+
+printTime(3)
+writeCSV("33")
