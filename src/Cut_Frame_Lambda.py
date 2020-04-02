ec943359ac2066e76eacb38cc44960e5c63ea768
xogns7868 <superman7868@naver.com>
2020-04-02 23:30:25 +0900

    S3 버킷 업로드 시 폴더 추가

--- src/Cut_Frame_Lambda.py
+++ src/Cut_Frame_Lambda.py
@@ -2,36 +2,56 @@ import boto3
 import os
 import sys
 import uuid
-from PIL import Image, ImageFilter
+import cv2
 
 s3_client = boto3.client('s3')
 
 TMP = "/tmp/"
+FILE_PATH_INDEX = 0
 FILE_NAME_INDEX = 2
 
-def augmentation_image(file_name, image_path):
+def video_processing(file_name, video_path):
     result_file_path = []
 
-    with Image.open(image_path) as image:
-        tmp = image 
-        result_path = "resized-"+file_name       
-        image.thumbnail((128, 128))
-        result_path = TMP+result_path
-        image.save(result_path)
+    video = cv2.VideoCapture(video_path)
+    idx = 0
+
+    while(video.isOpened()):
+        ret, frame = video.read()
+        width = video.get(3)
+        height = video.get(4)
+
+        if idx == 100:
+            break
+
+        if ret < 0:
+            break
+
+        frame = cv2.resize(frame, (720, 480))
+        result_path = video_path.split(".")[FILE_PATH_INDEX]+str(idx)+".jpg"
         result_file_path.append(result_path)
+
+        cv2.imwrite(result_path, frame)
+
+        idx += 1
+
+    video.release()
+    print ("Video Processing Done ...")
     return result_file_path
 
 def lambda_handler(event, context):
     for record in event['Records']:
         bucket = record['s3']['bucket']['name']
         key = record['s3']['object']['key']
-        print "bucket name : " + str(bucket)
-        print "key : " + str(key)
         download_path = '/tmp/{}{}'.format(uuid.uuid4(), key)
 
         s3_client.download_file(bucket, key, download_path)
         file_name = key
-        result_path = augmentation_image(file_name, download_path)
-        print result_path
+
+        result_path = video_processing(file_name, download_path)
+
         for upload_path in result_path:
-            s3_client.upload_file(upload_path, 'result-image-data-augmentation', upload_path.split("/")[FILE_NAME_INDEX])
+            s3_client.upload_file(upload_path, 'test7868-resized', upload_path.split("/")[FILE_NAME_INDEX])
+
+        print ("S3 Upload Done ...")
+
