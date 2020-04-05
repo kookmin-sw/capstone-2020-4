5891dde0c431b69b3f280445f6e93c2a864b9a4c
leejuhyoung11 <srlee96@naver.com>
2020-04-05 17:29:45 +0900

    Update makeData.py

--- src/makeData.py
+++ src/makeData.py
@@ -1,10 +1,19 @@
 import csv
 
 def writeCSV(sentence):
-    f = open('C:\\Users\\01097\\PycharmProjects\\untitled\\csv\\write.csv', 'a', newline='')
+    #f = open('C:\\Users\\01097\\PycharmProjects\\untitled\\csv\\write.csv', 'a', newline='')
+    f = open('/home/ubuntu/capstone-2020-4/src/csv/write.csv', 'a', newline='')
     wr = csv.writer(f)
     wr.writerow([sentence])
 
+
+    f.close()
+
+def writeTXT(sentence):
+    #f = open('C:\\Users\\01097\\PycharmProjects\\untitled\\csv\\write.txt', 'a')
+    f = open('/home/ubuntu/capstone-2020-4/src/csv/write.csv', 'a')
+    data = sentence + "\n"
+    f.write(data)
 
     f.close()
 
