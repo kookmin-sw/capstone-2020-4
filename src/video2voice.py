ee9f6b832d955e83dd3d9c342912e751c5ef7c8d
leejuhyoung11 <srlee96@naver.com>
2020-04-05 17:29:47 +0900

    Update video2voice.py

--- src/video2voice.py
+++ src/video2voice.py
@@ -2,8 +2,11 @@ import moviepy.editor as mp
 
 
 def video2voice(VIDEO_FILE):
-    clip = mp.VideoFileClip("C:\\Users\\01097\\PycharmProjects\\untitled\\video\\"+VIDEO_FILE)
-    clip.audio.write_audiofile("C:\\Users\\01097\\PycharmProjects\\untitled\\voice\\test10.wav")
+    #clip = mp.VideoFileClip("C:\\Users\\01097\\PycharmProjects\\untitled\\video\\"+VIDEO_FILE)
+    #clip.audio.write_audiofile("C:\\Users\\01097\\PycharmProjects\\untitled\\voice\\test10.wav")
+
+    clip = mp.VideoFileClip("/home/ubuntu/capstone-2020-4/src/video" + VIDEO_FILE)
+    clip.audio.write_audiofile("/home/ubuntu/capstone-2020-4/src/voice/test10.wav")
 
     print("finished extract voice")
 
