cc15d9bdef3a9bca361a064c40f37e6afd1d400b
xogns7868 <superman7868@naver.com>
2020-04-08 00:35:38 +0900

    5초에 한장씩 추출하는 것으로 변경

--- src/Cut_Frame_Lambda.py
+++ src/Cut_Frame_Lambda.py
@@ -2,6 +2,7 @@ import boto3
 import os
 import sys
 import uuid
+from urllib.parse import unquote_plus
 import cv2
 import socketio
 
@@ -11,30 +12,26 @@ TMP = "/tmp/"
 FILE_PATH_INDEX = 0
 FILE_NAME_INDEX = 2
 
+
 def video_processing(file_name, video_path):
     result_file_path = []
-
     video = cv2.VideoCapture(video_path)
     idx = 0
 
-    while(video.isOpened()):
+    while (video.isOpened()):
         ret, frame = video.read()
         width = video.get(3)
         height = video.get(4)
 
-        if idx == 100:
+        if (int(video.get(1)) % 150 == 0):
+            frame = cv2.resize(frame, (720, 480))
+            result_path = video_path.split(".")[FILE_PATH_INDEX] + str(idx) + ".jpg"
+            result_file_path.append(result_path)
+            cv2.imwrite(result_path, frame)
+            idx += 5
+
+        if not ret:
             break
-
-        if ret < 0:
-            break
-
-        frame = cv2.resize(frame, (720, 480))
-        result_path = video_path.split(".")[FILE_PATH_INDEX]+str(idx)+".jpg"
-        result_file_path.append(result_path)
-
-        cv2.imwrite(result_path, frame)
-
-        idx += 1
 
     video.release()
     print ("Video Processing Done ...")
@@ -42,22 +39,25 @@ def video_processing(file_name, video_path):
 
 def lambda_handler(event, context):
     for record in event['Records']:
+        print(record)
         bucket = record['s3']['bucket']['name']
-        key = record['s3']['object']['key']
+        key = unquote_plus(record['s3']['object']['key'])
         download_path = '/tmp/{}{}'.format(uuid.uuid4(), key)
-
         s3_client.download_file(bucket, key, download_path)
+        print ("jot")
         file_name = key
-
         result_path = video_processing(file_name, download_path)
-
+        tmpkey = key.split(".")
+        tmpframe = tmpkey[0]
+        idx = 0
         for upload_path in result_path:
-            s3_client.upload_file(upload_path, 'test7868-resized', upload_path.split("/")[FILE_NAME_INDEX])
+            s3_client.upload_file(upload_path, 'youhi-project', tmpframe + "/" + tmpframe + str(idx) + ".jpg")
+
+            idx += 5
 
         sio = socketio.Client()
         sio.connect('http://13.125.127.181:4567')
         sio.emit('msg', 'complete')
         sio.sleep(1)
         sio.disconnect()
-        print ("S3 Upload Done ...")
-
+        print("S3 Upload Done ...")
