5b070ad1a4d173e9c5e6ce5b70c71cb93c702a7c
xogns7868 <superman7868@naver.com>
2020-04-08 22:53:28 +0900

    소켓 통신 완료 후 child process 실행 추가

--- src/socket.js
+++ src/socket.js
@@ -1,19 +1,22 @@
-
 var io = require('socket.io').listen(4567);
-var roomName;
 var flag = 0;
+
 io.on('connection', function (socket) {
     console.log('connect');
     var instanceId = socket.id;
 
     socket.on('msg', function (data) {
         if(data === "complete"){
-          flag = 1;
-        }
-        if(flag == 1){
           console.log(data);
-          io.sockets.emit('msg', "jot");
-          flag = 0;
+        }
+        if(data === "isdone"){
+          console.log(data);
+          socket.emit('msg','done');
+          const { spawn } = require('child_process');
+          const ls = spawn('./a.out');
+          ls.stdout.on('data', (data) => {
+             console.log(`stdout: ${data}`);
+          });
         }
     })
 });
