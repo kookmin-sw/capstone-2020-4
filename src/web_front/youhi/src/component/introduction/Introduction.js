147aa8a323f47b97187832f4ee721ef02fc85466
inpyeong <jinipyung@gmail.com>
2020-04-10 12:28:17 +0900

    Implement Introduction text header and description

--- src/web_front/youhi/src/component/introduction/Introduction.js
+++ src/web_front/youhi/src/component/introduction/Introduction.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import './Introduction.css';
+import React from "react";
+import "./Introduction.css";
 
 const Introduction = () => {
   return (
     <div className="Introduction">
       <div className="Introduction-first-flex-items-container">
         <div className="Introduction-first-flex-items">
-          <div className="Introduction-text-header">
-            
-          </div>
+          <div className="Introduction-text-header">YouHi</div>
           <div className="Introduction-text-description">
-            
+            Video upload filtering application <br />
+            designed to provide safe video for minors.
           </div>
         </div>
       </div>
