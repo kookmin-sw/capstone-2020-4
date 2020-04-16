249e00f74f7a082b2f7bd0586985654e82271592
inpyeong <jinipyung@gmail.com>
2020-04-17 04:49:30 +0900

    Add onFilesAdded func to Dropzone component

--- src/web_front/youhi/src/component/upload/dropzone/Dropzone.js
+++ src/web_front/youhi/src/component/upload/dropzone/Dropzone.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import "./Dropzone.css"
+import React, { Component } from "react";
+import "./Dropzone.css";
 
 class Dropzone extends Component {
   constructor(props) {
@@ -8,21 +8,44 @@ class Dropzone extends Component {
     this.fileInputRef = React.createRef();
 
     // this.openFileDialog = this.openFileDialog.bind(this);
-    // this.onFilesAdded = this.onFilesAdded.bind(this);
+    this.onFilesAdded = this.onFilesAdded.bind(this);
     // this.onDragOver = this.onDragOver.bind(this);
     // this.onDragLeave = this.onDragLeave.bind(this);
     // this.onDrop = this.onDrop.bind(this);
   }
 
+  onFilesAdded(evt) {
+    if (this.props.disabled) return;
+    const files = evt.target.files; 
+    if (this.props.onFilesAdded) {
+      const array = this.fileListToArray(files);
+      this.props.onFilesAdded(array);
+    }
+  }
+
+  fileListToArray(list) {
+    const array = [];
+    for (var i = 0; i < list.length; i++) {
+      array.push(list.item(i));
+    }
+    return array;
+  }
+
   render() {
     return (
       <div className={`Dropzone ${this.state.highlight ? "Highlight" : ""}`}>
-        <input ref={this.fileInputRef}
-        className="Dropzone-FileInput"
-        type="file"
-        multiple
-        onChange={this.onFilesAdded}
-        / >
+        <input
+          ref={this.fileInputRef}
+          className="Dropzone-FileInput"
+          type="file"
+          multiple
+          onChange={this.onFilesAdded}
+        />
+        <span>
+          동영상을 '여기' 끌어다 놓거나
+          <br />
+          '여기'를 클릭하세요.
+        </span>
       </div>
     );
   }
