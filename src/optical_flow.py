49563d86a65916a7d43e0dfd3e2c3ad173f0029a
xogns7868 <superman7868@naver.com>
2020-04-19 07:12:09 +0900

    optical flow 전처리 코드 완성본

--- src/optical_flow.py
+++ src/optical_flow.py
@@ -1,53 +1,48 @@
 import cv2
 import numpy as np
-import torch
+import os
+from natsort import natsorted, ns
 # Create list of names here from my1.bmp up to my20.bmp
 
-list_names = ['my' + str(i+1) + '.bmp' for i in range(20)]
-
+dir_list = os.listdir("./jpegs_256/")
+dir_list = natsorted(dir_list)
+idx = 0
+for dir in dir_list:
+    dir_path = "./jpegs_256/" + dir +"/"
+    list_names = os.listdir(dir_path)
+    list_names = natsorted(list_names)
+    file_list = os.listdir(dir_path)
+    file_list = natsorted(file_list)
+    for i in range(0, len(file_list)):
+        list_names[i] = dir_path + list_names[i]
 # Read in the first frame
-frame1 = cv2.imread(list_names[0])
-prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+    
+    frame1 = cv2.imread(list_names[0])
+    prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
 
 # Set counter to read the second frame at the start
-counter = 1
+    counter = 1
 
 # Until we reach the end of the list...
-while counter < len(list_names):
+    while counter < len(list_names):
     # Read the next frame in
-    frame2 = cv2.imread(list_names[counter])
-    next = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
+        frame2 = cv2.imread(list_names[counter])
+        next = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
 
     # Calculate optical flow between the two frames
-    flow = cv2.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        flow = cv2.calcOpticalFlowFarneback(prvs, next, 1, 0.5, 3, 15, 3, 5, 1.2, 0)
 
     # Normalize horizontal and vertical components
-    horz = cv2.normalize(flow[...,0], None, 0, 255, cv2.NORM_MINMAX)
-    vert = cv2.normalize(flow[...,1], None, 0, 255, cv2.NORM_MINMAX)
-    horz = horz.astype('uint8')
-    vert = vert.astype('uint8')
-
+        horz = cv2.normalize(flow[...,0], None, 0, 255, cv2.NORM_MINMAX)     
+        vert = cv2.normalize(flow[...,1], None, 0, 255, cv2.NORM_MINMAX)
+        horz = horz.astype('uint8')
+        vert = vert.astype('uint8')
     # Show the components as images
-    cv2.imshow('Horizontal Component', horz)
-    cv2.imshow('Vertical Component', vert)
+        print(dir)
+        cv2.imwrite('u/'+dir + "/" +file_list[counter], horz)
+        cv2.imwrite('v/'+dir +"/" + file_list[counter], vert)
+        counter += 1
 
-    # Change - Make next frame previous frame
-    prvs = next.copy()
+    idx += 1 
 
-    # If we get to the end of the list, simply wait indefinitely
-    # for the user to push something
-    if counter == len(list_names)-1:
-        k = cv2.waitKey(0) & 0xff
-    else: # Else, wait for 1 second for a key
-        k = cv2.waitKey(1000) & 0xff
 
-    if k == 27:
-        break
-    elif k == ord('s'): # Change
-        cv2.imwrite('opticalflow_horz' + str(counter) + '-' + str(counter+1) + '.pgm', horz)
-        cv2.imwrite('opticalflow_vert' + str(counter) + '-' + str(counter+1) + '.pgm', vert)
-
-    # Increment counter to go to next frame
-    counter += 1
-
-cv2.destroyAllWindows()
