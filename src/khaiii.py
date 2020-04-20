82bf2a4e74593f11ddabe41abeccd916f607eab9
leejuhyoung11 <srlee96@naver.com>
2020-04-20 19:18:32 +0900

    khaiii test file 추가

--- src/khaiii.py
+++ src/khaiii.py
@@ -3,5 +3,18 @@ from khaiii import KhaiiiApi
 
 api = KhaiiiApi("", "")
 
-for word in api.analyze('?àÎÖï, ?∏ÏÉÅ.'):
-    print(word)
+def filterKhaiii(FILEPATH, DESTINATION):
+    f = open('FILEPATH', 'r')
+    des = open('DESTINATION', 'a')
+    while True:
+        line = f.readline()
+        for word in api.analyze(line):
+            f.write(word)
+        f.write("\n")
+
+        if line=="":
+            break
+
+
+
+
