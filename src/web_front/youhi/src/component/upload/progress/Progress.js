d08668afd59d44f145c7ab890e0f42b83d7b6135
inpyeong <jinipyung@gmail.com>
2020-04-23 23:35:41 +0900

    Add initial progress component

--- src/web_front/youhi/src/component/upload/progress/Progress.js
+++ src/web_front/youhi/src/component/upload/progress/Progress.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Component } from "react";
+import "./Progress.css";
 
-const Progress = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-};
+class Progress extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="ProgressBar">
+        <div
+          className="Progress"
+          style={{ width: this.props.progress + "%" }}
+        />
+      </div>
+    );
+  }
+}
 
 export default Progress;
