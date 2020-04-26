8c2659495eeb2191963e22f15d99d5530eb634d7
kwtjdtn <kwtjdtn1@naver.com>
2020-04-26 16:17:30 +0900

    필터 결과 컴포넌트 분리 및 정리

--- src/web_front/youhi/src/component/filter/Filter.js
+++ src/web_front/youhi/src/component/filter/Filter.js
@@ -5,69 +5,16 @@ import NewWindow from "react-new-window";
 import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import LoginModal from "../filter/exception/exception";
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
-}
+import FilterResult from "./FilterResult/FilterResult";
 
-function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-}
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    position: "absolute",
-    width: 400,
-    backgroundColor: theme.palette.background.paper,
-    border: "2px solid #000",
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-}));
 
 function Filter() {
-  const classes = useStyles();
-  // getModalStyle is not a pure function, we roll the style only on the first render
-  const [modalStyle] = React.useState(getModalStyle);
-  const [open, setOpen] = React.useState(false);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const body = (
-    <div style={modalStyle} className={classes.paper}>
-      Test
-    </div>
-  );
-
   return (
     <div className="Filter-container">
       <FilterElement />
       <div className="filter-wrapper">
         <div className="Filter-filter-box">
-          <div className="inner-filter-wrapper">
-            <button
-              className="Filter-box Filter-video-filter"
-              onClick={handleOpen}/>
-              <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="simple-modal-title"
-                aria-describedby="simple-modal-description">
-                {body}
-              </Modal>
-          </div>
+          {FilterResult()}
           <div className="filter-box-space"></div>
           <div className="inner-filter-wrapper">
             <button className="Filter-box Filter-voice-filter" />
