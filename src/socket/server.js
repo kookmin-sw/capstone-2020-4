813832951bccabccfb05a13b9db7d56587c8c40c
xogns7868 <superman7868@naver.com>
2020-04-30 18:27:12 +0900

    socket server

---
+++ src/socket/server.js
@@ -0,0 +1,39 @@
+
+var io = require('socket.io').listen(4567);
+var flag = new Array();
+var idx = 0;
+var clients = new Array();
+
+
+io.on('connection', function (socket) {
+    console.log(socket.id);
+    socket.on('ready', function (data) {
+         clients[idx] = socket.id;
+         console.log(socket.id);
+         console.log(data);
+         flag[idx] = data;
+         var tmp = data.split('.');
+         io.to(socket.id).emit('number',{value: String(idx) + '.' +  tmp[1]});
+         idx ++;
+    })
+    socket.on('complete', function (data) {
+         var id = data.split('.');
+
+         id = id[0];
+         console.log(data);
+         var a = "";
+         a = String(data);
+         io.to(clients[id]).emit("abc", "complete");
+         console.log("complete");
+        }
+        if(data === "isdone"){
+          console.log(data);
+          socket.emit('msg','done');
+          const { spawn } = require('child_process');
+          const ls = spawn('./a.out');
+          ls.stdout.on('data', (data) => {
+             console.log(`stdout: ${data}`);
+          });
+        }
+    })
+});
