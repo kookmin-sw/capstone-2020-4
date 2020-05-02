dc6c063023f28d160b5bd8bc275feb0e78e6c284
leejuhyoung11 <srlee96@naver.com>
2020-05-02 09:20:51 +0000

    modify stringmatch

--- src/khaiii_filter.py
+++ src/khaiii_filter.py
@@ -4,8 +4,8 @@ from khaiii import KhaiiiApi
 def khaiii_tokenize(corpus_fname, output_fname):
     api = KhaiiiApi()
 
-    with open(corpus_fname, 'r', encoding='utf-8') as f1, \
-            open(output_fname, 'w', encoding='utf-8') as f2:
+    with open(corpus_fname, 'r', encoding = 'utf-8') as f1, \
+            open(output_fname, 'w', encoding = 'utf-8') as f2:
         for line in f1:
             sentence = line.replace('\n', '').strip()
             tokens = api.analyze(sentence)
@@ -16,10 +16,7 @@ def khaiii_tokenize(corpus_fname, output_fname):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--preprocess_mode', type=str, help='preprocess mode')
     parser.add_argument('--input_path', type=str, help='Location of input files')
     parser.add_argument('--output_path', type=str, help='Location of output files')
     args = parser.parse_args()
-
-    if args.preprocess_mode == "khaiii_tokenize":
-        khaiii_tokenize(args.input_path, args.output_path)
+    khaiii_tokenize(args.input_path, args.output_path)
--- src/voice_filter.py
+++ src/voice_filter.py
@@ -10,21 +10,26 @@ def go(tokenized_khaiii_fname):
         cutPos = tokenList[i].find('/')
         tokenList[i] = tokenList[i][0:cutPos] #tokenList에는 한글 형태소만 들어있음.
       filter(tokenList)
+      #print(tokenList)
 
 def filter(tokenList):
-  swearList = ['시발', '씨발', '병신', '좆']
-  #model = fasttext.load_model('./model_skipgram.bin')
+  swearList = ['시발', '씨발', '병신', '좆','개새끼','씨발새끼','새끼','씨발놈','씹새끼','씨부랄','개씨발']
+  model = fasttext.load_model('/home/ubuntu/fastText/mywork/model_test.bin')
   for token in tokenList:
     for check in swearList:
-      similarity = cs_sim.calculate(model.get_word_vector(token), model.get_word_vector(check)))
+      similarity = cs_sim.calculate(model.get_word_vector(token), model.get_word_vector(check))
       if (similarity >= 0.7):
-        stringMatch(token, swearList)
+       stringMatch(token, swearList)
+       #print(token)
 
-def stringMatch(token, swearList):
+def stringMatch(token,swearList):
+  f = open('/home/ubuntu/capstone-2020-4/src/textData/result.txt','a')
   for swear in swearList:
     if swear in token:
-      print("Swear detected!")
-      sys.exit()
+      print(swear)
+      f.write(swear)
+      f.write('\n')
+      #sys.exit()
 
 
 if __name__ == '__main__':
