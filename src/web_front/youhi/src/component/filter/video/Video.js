4cd2cc2aab0de78f5c76e29d8da9a6d5c7f160af
kwtjdtn <kwtjdtn1@naver.com>
2020-05-18 18:44:27 +0900

    voice filter blur 처리

--- src/web_front/youhi/src/component/filter/video/Video.js
+++ src/web_front/youhi/src/component/filter/video/Video.js
@@ -1,69 +1,13 @@
-// import React from "react";
-// import { Scrollbars } from "react-custom-scrollbars";
-// import "./Video.css";
+import React , { Component , useState } from "react";
+import './Video.css';
 
-// const Video = () => {
-//   return (
-//     <div className="video-wrapper">
-//       <div id="videoFilter-title">영상 필터</div>
-//       <div id="videoFilter-box">
-//         <Scrollbars
-//           style={{ height: 420 }}
-//           renderTrackHorizontal={props => (
-//             <div
-//               {...props}
-//               style={{ display: "none" }}
-//               className="track-horizontal"
-//             />
-//           )}
-//         >
-//           <div>
-//             <div className="filteredVideo-order">1</div>
-//             <img
-//               className="filteredVideo"
-//               src={require("../img/filtered_video1.PNG")}
-//               alt=""
-//             />
-//           </div>
-//           <div>
-//             <div className="filteredVideo-order">2</div>
-//             <img
-//               className="filteredVideo"
-//               src={require("../img/filtered_video2.PNG")}
-//               alt=""
-//             />
-//           </div>
-//           <div>
-//             <div className="filteredVideo-order">3</div>
-//             <img
-//               className="filteredVideo"
-//               src={require("../img/filtered_video3.PNG")}
-//               alt=""
-//             />
-//           </div>
-//           <div>
-//             <div className="filteredVideo-order">4</div>
-//             <img
-//               className="filteredVideo"
-//               src={require("../img/filtered_video4.PNG")}
-//               alt=""
-//             />
-//           </div>
-//         </Scrollbars>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Video;
-
-
-import React from 'react';
-
-const Video = () => {
+const Video = props => {
   return (
-    <div>
-      
+    <div className="Video">
+      <button 
+        className="Filter-box Filter-video-filter"
+        disabled={!props.successfulFiltered}
+      />
     </div>
   );
 };
