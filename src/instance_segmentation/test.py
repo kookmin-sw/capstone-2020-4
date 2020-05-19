cd2091fe1fb055cdc7449939f498dc6d24a9db54
inpyeong <jinipyung@gmail.com>
2020-05-19 19:33:31 +0900

    Upload test.py

---
+++ src/instance_segmentation/test.py
@@ -0,0 +1,18 @@
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+import os
+import argparse
+import numpy as np
+import cv2
+import random
+from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2 import model_zoo
+from detectron2.engine import DefaultTrainer
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+from moviepy.tools import subprocess_call
+from moviepy.config import get_setting
+
