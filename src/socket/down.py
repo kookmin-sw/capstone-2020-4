f22e119668bc267f8bed8cd93349ec5d1c353dec
xogns7868 <superman7868@naver.com>
2020-05-24 00:55:42 +0900

    down.py 구현 완료

--- src/socket/down.py
+++ src/socket/down.py
@@ -8,9 +8,7 @@ print(str)
 
 os.system("aws s3 cp s3://uploadtest-s3/" + sys.argv[1] + " ./")
 os.system("mkdir " + str)
+os.system("mkdir ../static " + str)
+os.system("mkdir ../static/blood_result")
 os.system("aws s3 cp s3://youhi-project/" + str + " ./" + str+ "  --recursive")
-
-
-
-
-
+os.system("python3.6 segemtation.py --dir " + sys.argv[1])
