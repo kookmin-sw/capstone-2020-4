ea2fa19da4e19e9d2213a0afc50d00c03f7ef356
xogns7868 <superman7868@naver.com>
2020-05-24 00:54:34 +0900

    영상 검열 진행 서버 소켓 구현 완료

--- src/socket/server.js
+++ src/socket/server.js
@@ -3,7 +3,6 @@ var flag = new Array();
 var idx = 0;
 var clients = new Array();
 
-
 io.on('connection', function (socket) {
     console.log(socket.id);
     socket.on('ready', function (data) {
@@ -21,41 +20,18 @@ io.on('connection', function (socket) {
          console.log(data);
          var a = "";
          a = String(data);
-         io.to(clients[id]).emit("abc", "complete");
-         console.log("complete");
-         var exec = require('child_process').exec,
-              ls;
-          ls = exec("sudo python3.6 down.py " + data, function (error, stdout, stderr) {
-                console.log('stderr: ' + stderr);
-                if (error !== null) {
-                        console.log('exec error: ' + error);
-                }
-          });
-          console.log("done");
-
-       })
-
-    socket.on('msg', function (data) {
-        if(data === "complete"){
-          console.log(data);
-          var exec = require('child_process').exec,
-              ls;
-          ls = exec("python3.6 down.py " + data, function (error, stdout, stderr) {
-                console.log('stderr: ' + stderr);
-                if (error !== null) {
-                        console.log('exec error: ' + error);
-                }
-          });
-          console.log("done");
-        }
-        if(data === "isdone"){
-          console.log(data);
-          socket.emit('msg','done');
-          const { spawn } = require('child_process');
-          const ls = spawn('./a.out');
-          ls.stdout.on('data', (data) => {
-             console.log(`stdout: ${data}`);
-          });
-        }
+         io.to(clients[id]).emit("upload", "complete");
     })
+    socket.on('filter', function (data) {
+            console.log(data)
+            var exec = require('child_process').exec,
+              ls;
+            ls = exec("python3.6 down.py " + data, function (error, stdout, stderr) {
+                console.log('stderr: ' + stderr);
+                if (error !== null) {
+                        console.log('exec error: ' + error);
+                }
+            });
+            console.log("download done")
+   });
 });
