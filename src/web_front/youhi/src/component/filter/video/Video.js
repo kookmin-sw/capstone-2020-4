35e23cd00a4fa13be36439458703fdbe66de6368
inpyeong <jinipyung@gmail.com>
2020-05-29 21:59:57 +0900

    Implement parsing video filtering result

--- src/web_front/youhi/src/component/filter/video/Video.js
+++ src/web_front/youhi/src/component/filter/video/Video.js
@@ -16,6 +16,59 @@ class Video extends Component {
     };
   }
 
+  go = async () => {
+    var res,
+      data,
+      result = { labelArray: [], cntArray: [] };
+    res = await fetch("./static/0/adult_result.txt");
+    data = await res.text();
+    result = data.length ? this.collectLabelData(result, data, "adult") : this.addZero(result);
+
+    res = await fetch("./static/0/blood_result.txt");
+    data = await res.text();
+    result = data.length ? this.collectLabelData(result, data, "blood") : this.addZero(result);
+
+    res = await fetch("./static/0/knife_result.txt");
+    data = await res.text();
+    result = data.length ? this.collectLabelData(result, data, "knife") : this.addZero(result);
+
+    res = await fetch("./static/0/smoke_result.txt");
+    data = await res.text();
+    result = data.length ? this.collectLabelData(result, data, "smoke") : this.addZero(result);
+
+    this.setState(
+      {
+        result: result,
+        check: true,
+      },
+      () => {
+        console.log(this.state.result.labelArray);
+        console.log(this.state.result.cntArray);
+      }
+    );
+  };
+
+  addZero = (result) => {
+    var resultCopy = result;
+    resultCopy.cntArray.push(0);
+    return resultCopy;
+  }
+
+  collectLabelData = (result, data, label) => {
+    var resultCopy = result;
+    if (data[0] !== "<") {
+      const dataArray = data.split("\n");
+      var i = 0;
+      for (; i < dataArray.length; i++) {
+        resultCopy.labelArray[parseInt(dataArray[i])] = label;
+      }
+      resultCopy.cntArray.push(dataArray.length);
+    } else {
+      resultCopy.cntArray.push(0);
+    }
+    return resultCopy;
+  };
+
   openModal = () => {
     document.querySelector("body").style.overflow = "hidden";
     this.setState({ isModalOpen: true });
