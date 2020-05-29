fe0a34d802b99e224f8d3fc13fe3af59b29072fb
inpyeong <jinipyung@gmail.com>
2020-05-29 22:40:49 +0900

    Implement label boxes in video modal

--- src/web_front/youhi/src/component/filter/video/VideoModal.js
+++ src/web_front/youhi/src/component/filter/video/VideoModal.js
@@ -14,7 +14,30 @@ const VideoModal = ({ isOpen, close, result }) => {
           <div className="VideoModal-overlay" onClick={close} />
           <div className="VideoModal">
             <p className="title">영상 검열 결과</p>
-            <div className="content"></div>
+            <div className="content">
+            <div className="VideoModal-parallel-video-tabs">
+                <div className="VideoModal-tab">
+                  {getLabelsCnt(result.cntArray)}
+                  <div className="VideoModal-tab-subtitle">전체 결과</div>
+                </div>
+                <div className="VideoModal-tab">
+                  {result.cntArray[0]}
+                  <div className="VideoModal-tab-subtitle">Adult</div>
+                </div>
+                <div className="VideoModal-tab">
+                  {result.cntArray[1]}
+                  <div className="VideoModal-tab-subtitle">Blood</div>
+                </div>
+                <div className="VideoModal-tab">
+                  {result.cntArray[2]}
+                  <div className="VideoModal-tab-subtitle">Knife</div>
+                </div>
+                <div className="VideoModal-tab">
+                  {result.cntArray[3]}
+                  <div className="VideoModal-tab-subtitle">Smoke</div>
+                </div>
+              </div>
+            </div>
             <div className="button-wrap">
               <button onClick={close}>Confirm</button>
             </div>
@@ -32,3 +55,12 @@ const VideoModal = ({ isOpen, close, result }) => {
 };
 
 export default VideoModal;
+
+const getLabelsCnt = (array) => {
+  var i = 0;
+  var sum = 0;
+  for (; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+};
