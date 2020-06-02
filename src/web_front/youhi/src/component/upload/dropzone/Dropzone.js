48c6446fa5d5720ba06ffe9d66f8587a5b468eae
inpyeong <jinipyung@gmail.com>
2020-06-03 00:35:02 +0900

    Implement showing video thumbnail

--- src/web_front/youhi/src/component/upload/dropzone/Dropzone.js
+++ src/web_front/youhi/src/component/upload/dropzone/Dropzone.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import ReactPlayer from "react-player";
 import "./Dropzone.css";
 
 class Dropzone extends Component {
   constructor(props) {
     super(props);
-    this.state = { highlight: false };
+    this.state = { 
+      highlight: false,
+      fileUrl: null,
+    };
     this.fileInputRef = React.createRef();
+    this.inputDivRef = React.createRef();
 
     this.openFileDialog = this.openFileDialog.bind(this);
     this.onFilesAdded = this.onFilesAdded.bind(this);
@@ -21,7 +26,8 @@ class Dropzone extends Component {
 
   onFilesAdded(evt) {
     if (this.props.disabled) return;
-    const files = evt.target.files;
+    const files = evt.target.files; 
+    console.log("files", files);
     if (this.props.onFilesAdded) {
       const array = this.fileListToArray(files);
       this.props.onFilesAdded(array);
@@ -46,44 +52,70 @@ class Dropzone extends Component {
       const array = this.fileListToArray(files);
       this.props.onFilesAdded(array);
     }
-    this.setState({ highlight: false});
+    this.setState({ highlight: false });
   }
 
   fileListToArray(list) {
+    const fileUrl = window.URL.createObjectURL(list[0]);
     const array = [];
     for (var i = 0; i < list.length; i++) {
       array.push(list.item(i));
     }
+    this.setState({ fileUrl: fileUrl });
     return array;
+  }
+
+  renderView = () => {
+    const condition = this.props.filesLength ? true : false;
+    if (condition) {
+      return (
+        <ReactPlayer url={this.state.fileUrl} />
+      );
+    } else {
+      return (
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+          <img
+            alt="upload"
+            className="Icon"
+            src={require("../../../img/dropzone.png")}
+          />
+          <span>
+            동영상을 '여기' 끌어다 놓거나
+            <br />
+            '여기'를 클릭하세요.
+          </span>
+        </div>
+      );
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.props.filesLength) {
+      this.inputDivRef.current.style.border = "none";
+    } else {
+      this.inputDivRef.current.style.border = "1px dashed rgb(142, 123, 123)";
+    }
   }
 
   render() {
     return (
-      <div 
+      <div
         className={`Dropzone ${this.state.highlight ? "Highlight" : ""}`}
         onDragOver={this.onDragOver}
         onDragLeave={this.onDragLeave}
         onDrop={this.onDrop}
         onClick={this.openFileDialog}
         style={{ cursor: this.props.disabled ? "default" : "pointer" }}
+        ref={this.inputDivRef}
       >
         <input
           ref={this.fileInputRef}
-          className="Dropzone-FileInput"
+          className="FileInput"
           type="file"
           multiple
           onChange={this.onFilesAdded}
         />
-        <img
-          alt="dropzone img"
-          className="Icon"
-          src={require("../../../img/dropzone.png")}
-        />
-        <span>
-          동영상을 '여기' 끌어다 놓거나
-          <br />
-          '여기'를 클릭하세요.
-        </span>
+        {this.renderView()}
       </div>
     );
   }
