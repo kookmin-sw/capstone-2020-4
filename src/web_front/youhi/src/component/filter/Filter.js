1c317e4cc5ca046b5ccc9232c07e9ed9de8ab2de
inpyeong <jinipyung@gmail.com>
2020-06-03 02:07:56 +0900

    Implement dropdown

--- src/web_front/youhi/src/component/filter/Filter.js
+++ src/web_front/youhi/src/component/filter/Filter.js
@@ -1,63 +1,59 @@
 import React, { Component } from "react";
 import FilterElement from "./filter_elementor/FilterElementor";
 import "./Filter.css";
-// import NewWindow from "react-new-window";
-// import { makeStyles } from "@material-ui/core/styles";
-// import Modal from "@material-ui/core/Modal";
-// import LoginModal from "../filter/exception/exception";
-// import FilterResult from "./FilterResult/FilterResult";
-
-
-// function Filter() {
-//   return (
-//     <div className="Filter-container">
-//       <FilterElement />
-//       <div className="filter-wrapper">
-//         <div className="Filter-filter-box">
-//           {FilterResult()}
-//           <div className="filter-box-space"></div>
-//           <div className="inner-filter-wrapper">
-//             <button className="Filter-box Filter-voice-filter" />
-//           </div>
-//         </div>
-//         {LoginModal()}
-//       </div>
-
-//     </div>
-//   );
-// }
-
-// export default Filter;
-
-
 import VideoBox from "./video/Video";
 import VoiceBox from "./voice/Voice";
+import Feedback from "../feedback/Feedback";
 
 class Filter extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+    };
+    this.filterWrapperRef = React.createRef();
   }
+
+  notYetText = () => {
+    if (!this.props.showResult) {
+      return (
+        <div className="Filter-not-yet">
+          결과는 검열이 끝난 후 확인 가능합니다.
+        </div>
+      );
+    }
+  };
+
+  componentDidUpdate() {
+      const obj = this.filterWrapperRef.current;
+      obj.removeAttribute("style");
+      if (!this.props.showResult) 
+        obj.style.height = "0";
+      else 
+        obj.style.maxHeight = "800px";
+  }
+  
 
   render() {
     return (
       <div className="Filter-container">
         <FilterElement />
-        <div className="filter-wrapper">
+        {this.notYetText()}
+        <div
+          className={`${
+            this.props.showResult ? "Filter-wrapper" : "Filter-wrapper-closed"
+          }`}
+          ref={this.filterWrapperRef}
+        >
           <div className="Filter-filter-box">
             <div className="inner-filter-wrapper">
-              <VideoBox successfulFiltered={this.props.successfulFiltered}/>
+              <VideoBox />
             </div>
             <div className="inner-filter-wrapper">
-              <VoiceBox successfulFiltered={this.props.successfulFiltered}/>
+              <VoiceBox />
             </div>
           </div>
         </div>
-        {/* <div className="Filter-feedback-button-wrapper">
-          <button className="Filter-feedback-button">
-            이의신청 및 문의하기
-          </button>
-        </div> */}
+        <Feedback />
       </div>
     );
   }
