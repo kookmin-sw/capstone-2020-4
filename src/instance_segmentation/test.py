68a27b69e78357ecd9b78665addfad66d927676d
xogns7868 <superman7868@naver.com>
2020-06-04 21:13:44 +0900

    instance segmentation 실시 frame 개수 변경

--- src/instance_segmentation/test.py
+++ src/instance_segmentation/test.py
@@ -14,18 +14,26 @@ from detectron2.data import MetadataCatalog
 from moviepy.tools import subprocess_call
 from moviepy.config import get_setting
 import os
+import math
 import argparse
 import socketio
+from moviepy.editor import VideoFileClip
 import natsort
-
 ## get client ID
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--dir', type=str)
 args = parser.parse_args()
+clip = VideoFileClip(args.dir)
+duration = math.floor(clip.duration)
 client_folder = args.dir.split(".")[0]
+list = os.listdir(client_folder)
+length = len(list)
+frame_count = math.floor(length / duration)
+print(frame_count)
 os.system("mkdir " + client_folder + "/smoke")
 os.system("mkdir " + client_folder + "/knife")
-
+os.system("mkdir " + client_folder + "/adult")
 cfg = get_cfg()
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 5
 cfg.merge_from_file("detectron2_repo/configs/COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml")
@@ -38,11 +46,10 @@ knife_list = []
 smoke_list = []
 video_len = 0
 blood_text = open(client_folder + "/blood.txt", "w")
-adult_text = open(client_folder + "/adult_result.txt", "w")
 file_list = os.listdir(client_folder)
 filelist = [file for file in file_list if file.endswith(".jpg")]
 video_len = len(filelist)
-adult_check = 0
+#adult_check = 0
 file = 0
 while file < len(filelist):
     file_name = str(file) + ".jpg"
@@ -56,27 +63,26 @@ while file < len(filelist):
         print(label, score)
         #adult
         if label == 0 and score >= 0.75:
-            adult_check += 1
-            adult_text.write(str(file) + "\n")
-            file += 3
+#            adult_check += 1
+            cv2.imwrite(client_folder + "/adult_temp/" + str(int(file / frame_count)) + ".jpg)
+            file += frame_count
         # gun
         elif (label == 1 or label == 2) and score >= 0.7:
             blood_text.write(str(file) + ",")
-            file += 3
+            file += frame_count
         # knife
         elif label == 3 and score >= 0.75:
             knife_list.append(str(file))
-            file += 3
+            file += frame_count
         # smoke
         elif label == 4 and score >= 0.75:
             smoke_list.append(str(file))
-            file += 3
+            file += frame_count
     file += 1
-adult_text.close()
 blood_text.close()
 
-if adult_check >= 1:
-	os.system("mv " + client_folder + "/adult_result.txt ../static/" + client_folder)
+#if adult_check >= 1:
+#	os.system("mv " + client_folder + "/adult_result.txt ../static/" + client_folder) 
 smoke_list = natsort.natsorted(smoke_list)
 knife_list = natsort.natsorted(knife_list)
 
@@ -103,26 +109,28 @@ def ffmpeg_extract_subclip(filename, t1, t2, targetname=None):
 
 if len(knife_list) != 0:
     for x in range(len(knife_list) - 1):
-        video_name = knife_list[x]
-        ffmpeg_extract_subclip(args.dir, int(knife_list[x]), int(knife_list[x]) + 3, targetname=client_folder + f"/knife/{video_name}.mp4")
-    video_name = knife_list[len(knife_list)-1]
+        video_name = math.floor(int(knife_list[x])/frame_count)
+        ffmpeg_extract_subclip(args.dir, math.floor(int(knife_list[x])/frame_count), math.floor(int(knife_list[x])/frame_count) + 3, targetname=client_folder + f"/knife/{video_name}.mp4")
+    video_name = math.floor(int(knife_list[len(knife_list)-1])/frame_count)
     print(video_len)
-    if video_len - int(knife_list[len(knife_list)-1]) > 3:
-        ffmpeg_extract_subclip(args.dir, int(knife_list[len(knife_list)-1]), int(knife_list[len(knife_list)-1]) + 3, targetname=client_folder + f"/knife/{video_name}.mp4")
+    if duration - math.floor(int(knife_list[len(knife_list)-1])/frame_count) > 3:
+        ffmpeg_extract_subclip(args.dir, math.floor(int(knife_list[len(knife_list)-1])/frame_count), math.floor(int(knife_list[len(knife_list)-1])/frame_count) + 3, targetname=client_folder + f"/knife/{video_name}.mp4")
     else:
-        ffmpeg_extract_subclip(args.dir, int(knife_list[len(knife_list)-1]), video_len, targetname=client_folder + f"/knife/{video_name}.mp4")
+        ffmpeg_extract_subclip(args.dir, math.floor(int(knife_list[len(knife_list)-1])/frame_count), duration, targetname=client_folder + f"/knife/{video_name}.mp4")
     os.system("python3.6 flow.py --demo --label Knife --video " + client_folder +"/knife/ & python3.6 rgb.py --demo --label Knife --video " + client_folder + "/knife/")
     os.system("python3.6 fusion.py --video " + client_folder + "/knife/ --label knife")
 
 if len(smoke_list) != 0:
     print(smoke_list)
     for x in range(len(smoke_list) - 1):
-        video_name = smoke_list[x]
-        ffmpeg_extract_subclip(args.dir, int(smoke_list[x]), int(smoke_list[x]) + 3, targetname=client_folder + f"/smoke/{video_name}.mp4")
-    video_name = smoke_list[len(smoke_list)-1]
-    if video_len - int(smoke_list[len(smoke_list)-1]) > 3:
-        ffmpeg_extract_subclip(args.dir, int(smoke_list[len(smoke_list)-1]), int(smoke_list[len(smoke_list)-1]) + 3, targetname=client_folder + f"/smoke/{video_name}.mp4")
+        video_name = math.floor(int(smoke_list[x])/frame_count)
+        ffmpeg_extract_subclip(args.dir, math.floor(int(smoke_list[x])/frame_count), math.floor(int(smoke_list[x])/frame_count) + 3, targetname=client_folder + f"/smoke/{video_name}.mp4")
+    video_name = math.floor(int(smoke_list[len(smoke_list)-1])/frame_count)
+    if duration - math.floor(int(smoke_list[len(smoke_list)-1])/frame_count) > 3:
+        ffmpeg_extract_subclip(args.dir, math.floor(int(smoke_list[len(smoke_list)-1])/frame_count), math.floor(int(smoke_list[len(smoke_list)-1])/frame_count) + 3, targetname=client_folder + f"/smoke/{video_name}.mp4")
     else:
-        ffmpeg_extract_subclip(args.dir, int(smoke_list[len(smoke_list)-1]), video_len, targetname=client_folder + f"/smoke/{video_name}.mp4")
+        ffmpeg_extract_subclip(args.dir, math.floor(int(smoke_list[len(smoke_list)-1])/frame_count), duration, targetname=client_folder + f"/smoke/{video_name}.mp4") 
     os.system("python3.6 flow.py --demo --label smoke --video " + client_folder +"/smoke/ & python3.6 rgb.py --demo --label smoke --video " + client_folder + "/smoke/")
     os.system("python3.6 fusion.py --video " + client_folder + "/smoke/ --label smoke")
+
+os.system("rm -r " + client_folder)
