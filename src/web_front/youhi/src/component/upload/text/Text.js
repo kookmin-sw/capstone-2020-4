f830fae5fe2eb8166f0a43ef92e5b6ea0f260216
inpyeong <jinipyung@gmail.com>
2020-06-05 22:17:11 +0900

    Modify  Upload text

--- src/web_front/youhi/src/component/upload/text/Text.js
+++ src/web_front/youhi/src/component/upload/text/Text.js
@@ -1,35 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
+
 import "./Text.css";
 
-class Text extends Component {
-  render() {
-    return (
-      <div className="Text">
-        <div className="Text-text-title">도움말 및 제안사항</div>
-        <div className="Text-text-description">
-          <ul>
-            <li>
-              필터링에 소요되는 시간은 약 10분이며, 사용자의 인터넷 환경에 따라
-              달라질 수 있습니다.
-            </li>
-            <li>업로드 파일의 형식은 ,avi와 .mp4로 제한합니다.</li>
-            <li>
-              <div className="Text-text-emphasis">연령제한 옵션</div>을 선택하면
-              필터링없이 연령제한이 적용된 컨텐츠로 업로드됩니다.
-              <br />
-              <label className="Text-checkbox-container">
-                <div className="Text-text-emphasis">
-                  연령제한 컨텐츠로 설정합니다.
-                </div>
-                <input type="checkbox" className="Text-checkbox" />     
-                <div className="Text-checkmark"></div>           
-              </label>
-            </li>
-          </ul>
-        </div>
+const Text = () => {
+  return (
+    <div className="Text-wrapper">
+      <div className="Text-title">도움말 및 제안사항</div>
+      <div className="Text-box">
+        <ul>
+          <li>
+            필터링에 소요되는 시간은 약 10분이며, 사용자의 인터넷 환경에 따라
+            달라질 수 있습니다.
+          </li>
+          <li>재생 시간이 1분 미만인 영상만 업로드 가능합니다.</li>
+          <li>업로드 파일 형식은 mp4로 제한합니다.</li>
+          <li>
+            검열이 진행되는 동안 브라우저를 종료하거나 새로고침을 하면 모든 작업은 중지됩니다. 검열이 완료될 때까지 기다려주세요.
+          </li>
+        </ul>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Text;
