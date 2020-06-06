f9a33135925c142b7e075e8f4a639fd5be4e8b47
kwtjdtn <kwtjdtn1@naver.com>
2020-06-06 15:15:57 +0900

    Router를 위한 navbar 기능 추가

--- src/web_front/youhi/src/component/navbar/Navbar.js
+++ src/web_front/youhi/src/component/navbar/Navbar.js
@@ -1,11 +1,38 @@
 import React from "react";
 import "./Navbar.css";
+//import Logo from '../img/logo.png';
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="nav-wrapper">
-      <img id="main-logo" src={require("../../img/YouHi_logo.png")} alt="" />
-      <div id="project-name">YouHi</div>
+    <div className = "header-wrapper">
+      <div className = "main-header">
+        <div className="nav-wrapper">
+          <Link to='/'><img
+            id="main-logo"
+            src={require("../../img/YouHi_logo.png")}
+            alt=""
+          />
+          </Link>
+          <a href = "http://localhost:3000" className="nav-youhi">YouHi</a>
+          <div className = "nav-space"></div>
+          <NavLink
+            exact
+            to="/"
+            className="App-Nav"
+            activeStyle={{ fontWeight: "bold" , color: "white" }}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/skill"
+            className="App-Nav"
+            activeStyle={{ fontWeight: "bold" , color: "white" }}
+          >
+            Description
+          </NavLink>
+        </div>
+      </div>
     </div>
   );
 };
