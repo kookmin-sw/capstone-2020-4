3d945bab930fe21e337520a50ab8d091259bae50
inpyeong <jinipyung@gmail.com>
2020-06-08 04:00:34 +0900

    Implement modal showing result of voice and subtitle filter

--- src/web_front/youhi/src/component/filter/voice/Voice.js
+++ src/web_front/youhi/src/component/filter/voice/Voice.js
@@ -1,38 +1,106 @@
-// import React from "react";
-// import { Scrollbars } from "react-custom-scrollbars";
-// import "./Voice.css";
+import React, { Component } from "react";
+import VoiceModal from "./Modal";
+import "./Voice.css";
 
-// const Voice = () => {
-//   return (
-//     <div className="voice-wrapper">
-//       <div id="voiceFilter-title">음성 필터</div>
-//       <div id="voice-filter">
-//         <Scrollbars
-//           style={{ height: 356 }}
-//           renderTrackHorizontal={props => (
-//             <div
-//               {...props}
-//               style={{ display: "none" }}
-//               className="track-horizontal"
-//             />
-//           )}
-//         >
-//         </Scrollbars>
-//       </div>
-//     </div>
-//   );
-// };
+class Voice extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false,
+      check: false,
+      imgArray: [],
+      swearArray: [],
+    };
+  }
 
-// export default Voice;
+  go = async () => {
+    const clientID = this.props.clientID;
+    var res,
+      data,
+      swearArrayCopy = [],
+      imgArrayCopy = [];
+    res = await fetch(`./static/${clientID}/subtitle_result.txt`);
+    data = await res.text();
+    imgArrayCopy = data.length ? this.returnimgArray(data) : imgArrayCopy;
 
-import React from 'react';
+    res = await fetch(`./static/${clientID}/${clientID}_filter.txt`);
+    data = await res.text();
+    swearArrayCopy = data.length ? this.returnSwearArray(data) : swearArrayCopy;
 
-const Voice = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-};
+    this.setState({ imgArray: imgArrayCopy, swearArray: swearArrayCopy, check: true }, () => {
+      console.log("imgArray", this.state.imgArray);
+      console.log("swearArray", this.state.swearArray);
+    });
+  };
+
+  returnimgArray = (data) => {
+    var dataArray = data.split("\n");
+    var returnArray = [];
+    var i = 0;
+    if (data[0] !== "<") {
+      for (; i < data.length; i++) {
+        if (dataArray[i] !== "") {
+          returnArray.push(dataArray[i]);
+        }
+      }
+    }
+    return returnArray;
+  }
+
+  returnSwearArray = (data) => {
+    var copy = [];
+    if (data[0] !== "<") {
+      const dataArray = data.split("\n");
+      var i = 0;
+      for (; i < dataArray.length; i++) {
+        const dataArrayElement = dataArray[i].split("/");
+        console.log(dataArrayElement);
+        copy.push([[dataArrayElement[0].trim()], [dataArrayElement[1].trim()]])
+      }
+    } else {
+      copy.push("There's no result!");
+    }
+    return copy;
+  }
+
+  openModal = () => {
+    document.querySelector("body").style.overflow = "hidden";
+    this.setState({ isModalOpen: true });
+  };
+
+  closeModal = () => {
+    document.querySelector("body").style.overflow = "";
+    this.setState({ isModalOpen: false });
+  };
+
+  renderVoiceModal = () => {
+    if (this.state.check) {
+      return (
+        <VoiceModal
+          isOpen={this.state.isModalOpen}
+          close={this.closeModal}
+          imgArray={this.state.imgArray}
+          swearArray={this.state.swearArray}
+          clientID={this.props.clientID}
+        />
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div className="Voice">
+        <button
+          className="Filter-box Filter-voice-filter"
+          onClick={() => {
+            this.openModal();
+            this.go();
+          }}
+        ></button>
+        {this.renderVoiceModal()}
+      </div>
+    );
+  }
+}
 
 export default Voice;
