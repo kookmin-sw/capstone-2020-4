999a08e3ff3772e08e9c9099a8cb2eea5d6bd8a2
inpyeong <jinipyung@gmail.com>
2020-06-08 03:37:38 +0900

    Implement parsing according to client ID

--- src/web_front/youhi/src/component/filter/Filter.js
+++ src/web_front/youhi/src/component/filter/Filter.js
@@ -9,12 +9,14 @@ class Filter extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      socketConnection: false,
+
     };
     this.filterWrapperRef = React.createRef();
   }
 
   notYetText = () => {
-    if (!this.props.showResult) {
+    if (!this.props.showResult[0]) {
       return (
         <div className="Filter-not-yet">
           결과는 검열이 끝난 후 확인 가능합니다.
@@ -26,7 +28,7 @@ class Filter extends Component {
   componentDidUpdate() {
       const obj = this.filterWrapperRef.current;
       obj.removeAttribute("style");
-      if (!this.props.showResult) 
+      if (!this.props.showResult[0]) 
         obj.style.height = "0";
       else 
         obj.style.maxHeight = "800px";
@@ -40,16 +42,16 @@ class Filter extends Component {
         {this.notYetText()}
         <div
           className={`${
-            this.props.showResult ? "Filter-wrapper" : "Filter-wrapper-closed"
+            this.props.showResult[0] ? "Filter-wrapper" : "Filter-wrapper-closed"
           }`}
           ref={this.filterWrapperRef}
         >
           <div className="Filter-filter-box">
             <div className="inner-filter-wrapper">
-              <VideoBox />
+              <VideoBox clientID={this.props.showResult[1]} />
             </div>
             <div className="inner-filter-wrapper">
-              <VoiceBox />
+              <VoiceBox clientID={this.props.showResult[1]} />
             </div>
           </div>
         </div>
