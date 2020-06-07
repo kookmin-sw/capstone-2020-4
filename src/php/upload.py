0229c182fda8cc5ddeb18cf3f637fac8f7596d76
xogns7868 <superman7868@naver.com>
2020-06-07 22:47:29 +0900

    ip 주소 삭제

--- src/php/upload.py
+++ src/php/upload.py
@@ -8,7 +8,7 @@ parser.add_argument('--dir', type=str,
 args = parser.parse_args()
 
 try:
-    url = 'http://ec2-13-209-93-181.ap-northeast-2.compute.amazonaws.com/YouHi-Wep/upload.php'
+    url = 'your-instance-url/upload.php'
     files = {'myfile': open(args.dir, 'rb')}
     r = requests.post(url, files=files)
 except:
