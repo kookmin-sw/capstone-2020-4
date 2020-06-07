e4a764af67d20c1b772ab761fd49895b7f297511
xogns7868 <superman7868@naver.com>
2020-06-07 22:41:39 +0900

    서버 인스턴스 간 다른 파일로 생성

--- src/socket/done.py
+++ src/socket/done.py
@@ -9,7 +9,7 @@ parser.add_argument('--dir', type=str,
 args = parser.parse_args()
 
 sio = socketio.Client()
-sio.connect('http://13.209.93.181:4567')
+sio.connect('put video ip address and port num')
 sio.emit('voice', args.dir)
 sio.sleep(1)
 sio.disconnect()
