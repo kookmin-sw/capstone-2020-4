f705f0d3fd515e6372b8fd2676273b0c1b6cfe5c
inpyeong <jinipyung@gmail.com>
2020-06-09 08:42:11 +0900

    Correct instance-segmentation component

--- src/web_front/youhi/src/skill.js
+++ src/web_front/youhi/src/skill.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import Introduction from "./component/introduction/Introduction";
 import Videoflow from "./component/skill/videoflow";
 import Footer from "./component/footer/footer";
-import Object_detection from "./component/skill/object-detection";
-// import Instance_segmentation from "./component/skill/instance-segmentation";
+import ObjectDetection from "./component/skill/object-detection";
+import InstanceSegmentation from "./component/skill/instance-segmentation";
 // import Image_classification from "./component/skill/image-classification";
 // import STT from "./component/skill/stt";
 // import Web from "./component/skill/web";
@@ -13,9 +13,9 @@ const Skill =() => {
         <div>
             <Introduction/>
             <Videoflow/>
-            <Object_detection/>
-            {/* <Instance_segmentation/>
-            <Image_classification/>
+            <ObjectDetection/>
+            <InstanceSegmentation/>
+            {/* <Image_classification/>
             <STT/>
             <Web/> */}
             <Footer/>
