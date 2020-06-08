64af945f38820083587479e0b3908ae0c8949321
inpyeong <jinipyung@gmail.com>
2020-06-09 02:33:54 +0900

    Modify position of nav items

--- src/web_front/youhi/src/component/navbar/Navbar.js
+++ src/web_front/youhi/src/component/navbar/Navbar.js
@@ -5,32 +5,38 @@ import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className = "header-wrapper">
-      <div className = "main-header">
+    <div className="header-wrapper">
+      <div className="main-header">
         <div className="nav-wrapper">
-          <Link to='/'><img
-            id="main-logo"
-            src={require("../../img/YouHi_logo.png")}
-            alt=""
-          />
-          </Link>
-          <a href = "http://localhost:3000" className="nav-youhi">YouHi</a>
-          <div className = "nav-space"></div>
-          <NavLink
-            exact
-            to="/"
-            className="App-Nav"
-            activeStyle={{ fontWeight: "bold" , color: "white" }}
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/description"
-            className="App-Nav"
-            activeStyle={{ fontWeight: "bold" , color: "white" }}
-          >
-            Description
-          </NavLink>
+          <div className="nav-flex-first-items">
+            <Link to="/">
+              <img
+                id="main-logo"
+                src={require("../../img/YouHi_logo.png")}
+                alt=""
+              />
+            </Link>
+            <a href="http://localhost:3000" className="nav-youhi">
+              YouHi
+            </a>
+          </div>
+          <div className="nav-flex-second-items">
+            <NavLink
+              exact
+              to="/"
+              className="App-Nav"
+              activeStyle={{ fontWeight: "bold", color: "white" }}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/description"
+              className="App-Nav"
+              activeStyle={{ fontWeight: "bold", color: "white" }}
+            >
+              Description
+            </NavLink>
+          </div>
         </div>
       </div>
     </div>
