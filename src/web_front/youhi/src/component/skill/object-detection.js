7601f90de126d56b8ae6be6f57418b5bd1c38eae
inpyeong <jinipyung@gmail.com>
2020-06-09 08:41:38 +0900

    Remove unnecessary code line

--- src/web_front/youhi/src/component/skill/object-detection.js
+++ src/web_front/youhi/src/component/skill/object-detection.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "./object-detection.css";
-import object1 from "../../img/object1.png";
 import rcnn from "../../img/rcnn.png";
 
 class Object_detection extends Component {
@@ -20,8 +19,7 @@ class Object_detection extends Component {
                 뜻한다. 각 관심 영역(RoI; Region of Interest)에 대한 피쳐 추출의
                 계산을 공유하고 딥러닝 기반의 RPN을 도입해 구현한다. 이는 단일
                 단계 방식보다 빠른 처리가 된다는 뜻이 아닌 이전 버전이라 할 수
-                있는 RCNN 알고리즘과 Fast RCNN Faster RCNN 알고리즘 이름에
-                ‘빠른(Faster)’이라는 단어가 포함되어 있지만
+                있는 RCNN 알고리즘과 Fast RCNN Faster RCNN 
               </p>
             </div>
             <div className="Object-detection-image">
