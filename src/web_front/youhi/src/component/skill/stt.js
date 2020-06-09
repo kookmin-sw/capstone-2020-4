469508b8a7dac851cf91c6177ff78646cbd37f99
inpyeong <jinipyung@gmail.com>
2020-06-09 14:54:43 +0900

    Change img

--- src/web_front/youhi/src/component/skill/stt.js
+++ src/web_front/youhi/src/component/skill/stt.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./stt.css";
-import stt_img from "../../img/stt.PNG";
+import stt_img from "../../img/stt.png";
 
 class STT extends Component {
   render() {
@@ -8,6 +8,9 @@ class STT extends Component {
       <div className="STT">
         <div className="STT-container-wrapper">
           <div className="STT-container">
+            <div className="STT-image">
+              <img src={stt_img} alt="" />
+            </div>
             <div className="STT-title-text-container">
               <div className="STT-title">STT</div>
               <div className="STT-title-bar" />
@@ -17,17 +20,7 @@ class STT extends Component {
                 문자 정보를 출력한다. 언어모델은 단어 시퀀스에 확률을
                 할당(assign) 하는 일을 하는 모델이다. 이는 가장 자연스러운 단어
                 시퀀스를 찾는 역할을 한다. 음향모델은 언어의 소리단위를 딥러닝을
-                통해 학습하여 어떤 단어가 어떤 소리로 나는지를 확률적으로
-                변환한다.
-              </p>  
-              <div className="STT-image">
-                <img src={stt_img} alt="" />
-              </div>
-              <p className="STT-text">
-                이 두가지 모델이 동시에 작용하여 높은 확률을 보인 단어를
-                출력한다. STT(Speech To Text) 는 잡음처리와 특징 추출과정을 거친
-                음성 데이터가 언어모델과 음향모델이 결합된 디코더를 통과한 후
-                문장 형태로 출력되면 이를 텍스트로 저장한다.
+                통해 학습하여 어떤 단어가 어떤 소리로 나는지를 확률적으
               </p>
             </div>
           </div>
